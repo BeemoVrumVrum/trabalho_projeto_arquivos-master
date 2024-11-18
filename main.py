984c63c7b191948461f15219f5a0f19f3655ddd6
BeemoVrumVrum <131203879+BeemoVrumVrum@users.noreply.github.com>
2024-11-18 14:07:21 -0300

    main.py
    alteração na ordem de qtd_operaçoes para dentro do loop

--- main.py
+++ main.py
@@ -14,61 +14,61 @@ def main():
     arvore_avl = ArvoreAVL()
 
     # Adição - AVL
-    qtd_operacoes = [0]
     for i in range(NUM_CONJUNTOS):
+        qtd_operacoes = [0]
         for j in range(MAX_CHAVES):
             arvore_avl.adicionarAVL(numeros[i][j], qtd_operacoes)
         print(f"Adição AVL #{i + 1} - {qtd_operacoes[0]}")
 
     # Remoção - AVL
-    qtd_operacoes = [0]
     for i in range(NUM_CONJUNTOS):
+        qtd_operacoes = [0]
         for j in range(MAX_CHAVES):
-            qtd_operacoes += arvore_avl.removerAVL(numeros[i][j], qtd_operacoes)
+            arvore_avl.removerAVL(numeros[i][j], qtd_operacoes)
         print(f"Remoção AVL #{i + 1} - {qtd_operacoes[0]}")
 
     # Árvore B (ordem 1)
     arvore_b1 = ArvoreB(1)
-    qtd_operacoes = [0]
     for i in range(NUM_CONJUNTOS):
+        qtd_operacoes = [0]
         for j in range(MAX_CHAVES):
-            qtd_operacoes += arvore_b1.adicionaChaveB(numeros[i][j], qtd_operacoes)
+            arvore_b1.adicionaChaveB(numeros[i][j], qtd_operacoes)
         print(f"Adição B1 #{i + 1} - {qtd_operacoes[0]}")
 
     # Árvore B (ordem 5)
     arvore_b5 = ArvoreB(5)
-    qtd_operacoes = [0]
     for i in range(NUM_CONJUNTOS):
+        qtd_operacoes = [0]
         for j in range(MAX_CHAVES):
-            qtd_operacoes += arvore_b5.adicionaChaveB(numeros[i][j], qtd_operacoes)
+            arvore_b5.adicionaChaveB(numeros[i][j], qtd_operacoes)
         print(f"Adição B5 #{i + 1} - {qtd_operacoes[0]}")
 
     # Árvore B (ordem 10)
     arvore_b10 = ArvoreB(10)
-    qtd_operacoes = [0]
     for i in range(NUM_CONJUNTOS):
+        qtd_operacoes = [0]
         for j in range(MAX_CHAVES):
-            qtd_operacoes += arvore_b10.adicionaChaveB(numeros[i][j], qtd_operacoes)
+            arvore_b10.adicionaChaveB(numeros[i][j], qtd_operacoes)
         print(f"Adição B10 #{i + 1} - {qtd_operacoes[0]}")
     # Remoção - B (ordem 1)
-    qtd_operacoes_b1 = [0]
     for conjunto in range(NUM_CONJUNTOS):
+        qtd_operacoes_b1 = [0]
         for j in range(MAX_CHAVES):
-            qtd_operacoes_b1[0] += arvore_b1.removeChaveB(numeros[conjunto][j], qtd_operacoes_b1)
+            arvore_b1.removeChaveB(numeros[conjunto][j], qtd_operacoes_b1)
         print(f"Remoção B1 #{conjunto + 1} - {qtd_operacoes_b1[0]}")
 
     # Remoção - B (ordem 5)
-    qtd_operacoes_b5 = [0]
     for conjunto in range(NUM_CONJUNTOS):
+        qtd_operacoes_b5 = [0]
         for j in range(MAX_CHAVES):
-            qtd_operacoes_b5[0] += arvore_b5.removeChaveB(numeros[conjunto][j], qtd_operacoes_b5)
+            arvore_b5.removeChaveB(numeros[conjunto][j], qtd_operacoes_b5)
         print(f"Remoção B5 #{conjunto + 1} - {qtd_operacoes_b5[0]}")
 
     # Remoção - B (ordem 10)
-    qtd_operacoes_b10 = [0]
     for conjunto in range(NUM_CONJUNTOS):
+        qtd_operacoes_b10 = [0]
         for j in range(MAX_CHAVES):
-            qtd_operacoes_b10[0] += arvore_b10.removeChaveB(numeros[conjunto][j], qtd_operacoes_b10)
+            arvore_b10.removeChaveB(numeros[conjunto][j], qtd_operacoes_b10)
         print(f"Remoção B10 #{conjunto + 1} - {qtd_operacoes_b10[0]}")
 
     # Soma total de operações de remoção
@@ -77,17 +77,17 @@ def main():
 
     # Árvore RN
     arvore_rn = ArvoreRN()
-    qtd_operacoes = [0]
     for i in range(NUM_CONJUNTOS):
+        qtd_operacoes = [0]
         for j in range(MAX_CHAVES):
-            qtd_operacoes += arvore_rn.adicionarRN(numeros[i][j], qtd_operacoes)
+            arvore_rn.adicionarRN(numeros[i][j], qtd_operacoes)
         print(f"Adição RN #{i + 1} - {qtd_operacoes[0]}")
 
     # Remoção - RN
-    qtd_operacoes = [0]
     for i in range(NUM_CONJUNTOS):
+        qtd_operacoes = [0]
         for j in range(MAX_CHAVES):
-            qtd_operacoes += arvore_rn.removerRN(numeros[i][j], qtd_operacoes)
+            arvore_rn.removerRN(numeros[i][j], qtd_operacoes)
         print(f"Remoção RN #{i + 1} - {qtd_operacoes[0]}")
 
 if __name__ == "__main__":
